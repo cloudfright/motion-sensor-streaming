18891b57bd530eca9dbb5c692db12716a5803d01
Ben Dyer <ben.dyer@hey.com>
2022-03-05 08:04:10 +0000

    Echo test message

--- api/message/index.js
+++ api/message/index.js
@@ -1,9 +1,7 @@
 module.exports = async function (context, req) {
 
-    context.log(context.req.body);
-
     context.res.json({
-        text: "Hello from the API"
+        text: context.req.body
     });
 };
 
