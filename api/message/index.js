ab51d5776d12348566d50dc58a7b09b18923e610
Ben Dyer <ben.dyer@hey.com>
2022-03-06 07:45:30 +0000

    Test table bindings

--- api/message/index.js
+++ api/message/index.js
@@ -1,23 +1,8 @@
 
-const storage = require('@azure/data-tables');
-const client = TableClient.fromConnectionString(TABLE_STORAGE_CONNECTION_STRING, "motioncap");
-
-
 module.exports = async function (context, req) {
 
-    await client.createEntity(req.body);
-
-    // Use { echoContent: true } if you want to return the created item including the Timestamp & etag
-    tableService.insertEntity(tableName, item, { echoContent: true }, function (error, result, response) {
-        if (!error) {
-            // This returns a 201 code + the database response inside the body
-            // Calling status like this will automatically trigger a context.done()
-            context.res.status(201).json(response);
-        } else {
-            // In case of an error we return an appropriate status code and the error returned by the DB
-            context.res.status(500).json({ error: error });
-        }
-    });
+    context.bindings.tableBinding = [];
+    context.bindings.tableBinding.push(req.body);
 };
 
 // https://github.com/Azure/azure-sdk-for-js/blob/main/sdk/tables/data-tables/MigrationGuide.md
